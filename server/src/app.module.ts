85817a0c0c759a0fab2c92dba87db7e617b3e534
Ren0503 <illuminati0440@gmail.com>
2022-01-14 22:23:06 +0700

    complete server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { DiscoverModule } from './discover/discover.module';
@@ -9,10 +7,44 @@ import { DislikeModule } from './dislike/dislike.module';
 import { MatchModule } from './match/match.module';
 import { MemberModule } from './member/member.module';
 import { MessageModule } from './message/message.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
-  imports: [UserModule, AuthModule, DiscoverModule, LikeModule, DislikeModule, MatchModule, MemberModule, MessageModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UserModule,
+    AuthModule,
+    DiscoverModule,
+    LikeModule,
+    DislikeModule,
+    MatchModule,
+    MemberModule,
+    MessageModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.HOST,
+      url: process.env.DATABASE_URL,
+      port: 5432,
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      extra: {
+        ssl: true
+      }
+    }),
+    GraphQLModule.forRoot({
+      context: ({ req }) => ({ req }),
+      typePaths: ['./**/*.graphql'],
+      installSubscriptionHandlers: true,
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.schema.ts'),
+        outputAs: 'class'
+      },
+      path: '/'
+    })
+  ],
 })
-export class AppModule {}
+export class AppModule { }
--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 4000);
 }
-bootstrap();
+
+// wait for db to accept connection
+setTimeout(async () => await bootstrap(), 5000)
